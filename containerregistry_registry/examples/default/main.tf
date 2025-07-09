9c67b1e396e36003d5f1209b8b2d5330e3f96ed2
khaledrazouk <94979673+khaledrazouk@users.noreply.github.com>
2025-07-09 21:54:50 +0200

    Add containerregistry_registry/examples/default/main.tf

---
+++ containerregistry_registry/examples/default/main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_version = "~> 1.6"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 4, < 5.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+# This ensures we have unique CAF compliant names for our resources.
+module "naming" {
+  source  = "Azure/naming/azurerm"
+  version = "0.4.0"
+}
+
+# This is required for resource modules
+resource "azurerm_resource_group" "this" {
+  location = "australiaeast"
+  name     = module.naming.resource_group.name_unique
+}
+
+# This is the module call
+module "containerregistry" {
+  source = "../../"
+  # source             = "Azure/avm-containerregistry-registry/azurerm"
+  name                = module.naming.container_registry.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+}
